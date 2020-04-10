24e388a87b0a9752c54d396c1eeaac920053e6b4
Michail <hushokas@gmail.com>
2020-04-10 15:26:58 +0300

    []css: configuring css module support, browserlist

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = required('path');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
     mode: 'development',
@@ -15,6 +16,24 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' }, /* injecting css code into html file */
+                    { loader: 'css-loader', options: { /* understand css imports, add conf to support modules */
+                        importLoaders: 1,
+                        modules: {
+                            localIdentName: '[name]_[local]_[hash:base64:5]'
+                        }
+                    } 
+                },
+                { loader: 'postcss-loader', oprions: { /* processes css code automatically prefexises, to work in older browsers */
+                    ident: 'postcss',
+                    plugins: () => [autoprefixer()] /* add browserlist in package.json  */
+                }} 
+                ]
             }
         ]
     }
