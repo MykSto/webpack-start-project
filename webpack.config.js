0afad3a9b5a1c9c1bb632142e2e129e7a4a7157b
Michail <hushokas@gmail.com>
2020-04-10 14:51:04 +0300

    []config: webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = required('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: ''
+    },
+    devTool: 'cheap-module-eval-source-map'
+};
